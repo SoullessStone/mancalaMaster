2f881042efb2dad83ea805d2ab64539f4241dc13
SoullessStone <micheldc@windowslive.com>
2018-04-04 19:38:19 +0200

    added basic game

---
+++ game.py
@@ -0,0 +1,52 @@
+from gamemodel import GameModel
+
+class Game:
+    __turn = 0;
+    __MAX_TURN = 0;
+    __MIN_TURN = 1;
+
+    # Returns, if it is the turn of MAX
+    def isMaxTurn(self):
+        return self.__turn == self.__MAX_TURN;
+    
+    # Returns, if it is the turn of MIN
+    def isMinTurn(self):
+        return self.__turn == self.__MIN_TURN;
+    
+    # Returns the initial state
+    def start(self):
+        return GameModel();
+    
+    # Do a move on current state
+    def doMove(self, move):
+        raise NotImplementedError("You should have implemented this");
+    
+    # Returns the last move leading to this state
+    def getLastMove(self):
+        raise NotImplementedError("You should have implemented this");
+    
+    # Returns all possible moves of this state
+    def getPossibleMoves(self):
+        raise NotImplementedError("You should have implemented this");
+    
+    # Returns all successor states
+    def expand(self):
+        raise NotImplementedError("You should have implemented this");
+    
+    # Returns, if the current state is terminal
+    def isTerminal(self):
+        raise NotImplementedError("You should have implemented this");
+    
+    # Returns the evaluation of a state related to MAX
+    def evalValueForMax(self):
+        raise NotImplementedError("You should have implemented this");
+    
+    # Returns the evaluation for a terminal state (for MAX)
+    def utility(self):
+        raise NotImplementedError("You should have implemented this");
+    
+
+
+
+
+print(Game().start());
