d5186ff135f5f9a53cac171f4cf6567e684d4342
SoullessStone <micheldc@windowslive.com>
2018-04-05 22:07:07 +0200

    made a mistake and changed gameview, reverted this file to old version

--- gameview.py
+++ gameview.py
@@ -1,125 +1,74 @@
+from tkinter import *;
+from game import Game;
 
-        self.assertFalse(result)
 
-    def test_isLegalMove_Player2_0(self):
-        sut = Game();
-        sut.gameModel.switchTurn();
-        print(sut.isMinTurn());
-        print("bla");
+    
+# Wie kann man das Model updaten?
+class GameView:
+    fenster = None;
+    game = Game();
+    
+        
+    def updateView(self):
+        self.points_p1.config(text=str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER1_BASE)));
+        self.points_p2.config(text=str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER2_BASE)));
+        
+        self.p1_1["text"] = str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER1_1));
+        self.p1_2["text"] = str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER1_2));
+        self.p1_3["text"] = str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER1_3));
+        self.p1_4["text"] = str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER1_4));
+        self.p1_5["text"] = str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER1_5));
+        self.p1_6["text"] = str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER1_6));
 
-        result = sut.isLegalMove(0);        
+        self.p2_1["text"] = str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER2_1));
+        self.p2_2["text"] = str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER2_2));
+        self.p2_3["text"] = str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER2_3));
+        self.p2_4["text"] = str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER2_4));
+        self.p2_5["text"] = str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER2_5));
+        self.p2_6["text"] = str(self.game.gameModel.getFieldValue(self.game.gameModel.PLAYER2_6));
+        self.fenster.after(2,self.updateView);
 
-        print("isLegalMove: " + str(result))
-        self.assertFalse(result)
+    def doPlayStep(self,button):
+        # Do Play in Game
+        self.game.gameModel.changeFieldValue(button,button);
+        
+    def __init__(self):
+        self.fenster = Tk();
+        self.fenster.title("Mancala");
+        self.fenster.after(2,self.updateView);
+        
+        self.p1_1 = Button(self.fenster, text="0", command=lambda: self.doPlayStep(self.game.gameModel.PLAYER1_1));
+        self.p1_2 = Button(self.fenster, text="0", command=lambda: self.doPlayStep(self.game.gameModel.PLAYER1_2));
+        self.p1_3 = Button(self.fenster, text="0", command=lambda: self.doPlayStep(self.game.gameModel.PLAYER1_3));
+        self.p1_4 = Button(self.fenster, text="0", command=lambda: self.doPlayStep(self.game.gameModel.PLAYER1_4));
+        self.p1_5 = Button(self.fenster, text="0", command=lambda: self.doPlayStep(self.game.gameModel.PLAYER1_5));
+        self.p1_6 = Button(self.fenster, text="0", command=lambda: self.doPlayStep(self.game.gameModel.PLAYER1_6));
 
-    def test_isLegalMove_Player2_1(self):
-        sut = Game();
+        self.p2_1 = Button(self.fenster, text="0", command=lambda: self.doPlayStep(self.game.gameModel.PLAYER2_1));
+        self.p2_2 = Button(self.fenster, text="0", command=lambda: self.doPlayStep(self.game.gameModel.PLAYER2_2));
+        self.p2_3 = Button(self.fenster, text="0", command=lambda: self.doPlayStep(self.game.gameModel.PLAYER2_3));
+        self.p2_4 = Button(self.fenster, text="0", command=lambda: self.doPlayStep(self.game.gameModel.PLAYER2_4));
+        self.p2_5 = Button(self.fenster, text="0", command=lambda: self.doPlayStep(self.game.gameModel.PLAYER2_5));
+        self.p2_6 = Button(self.fenster, text="0", command=lambda: self.doPlayStep(self.game.gameModel.PLAYER2_6));
 
-        result = sut.isLegalMove(1);        
+        self.points_p1 = Label(self.fenster, text="0");
+        self.points_p2 = Label(self.fenster, text="0");
 
-        print("isLegalMove: " + str(result))
-        self.assertFalse(result)
+        self.p2_6.grid(row=0, column=0, pady = 20);
+        self.p1_1.grid(row=1, column=0, pady = 20);
+        self.p2_5.grid(row=0, column=1, pady = 20);
+        self.p1_2.grid(row=1, column=1, pady = 20);
+        self.p2_4.grid(row=0, column=2, pady = 20);
+        self.p1_3.grid(row=1, column=2, pady = 20);
+        self.p2_3.grid(row=0, column=3, pady = 20);
+        self.p1_4.grid(row=1, column=3, pady = 20);
+        self.p2_2.grid(row=0, column=4, pady = 20);
+        self.p1_5.grid(row=1, column=4, pady = 20);
+        self.p2_1.grid(row=0, column=5, pady = 20);
+        self.p1_6.grid(row=1, column=5, pady = 20);
+        
+        self.fenster.mainloop();
 
-    def test_isLegalMove_Player2_2(self):
-        sut = Game();
 
-        result = sut.isLegalMove(2);        
 
-        print("isLegalMove: " + str(result))
-        self.assertFalse(result)
-
-    def test_isLegalMove_Player2_3(self):
-        sut = Game();
-
-        result = sut.isLegalMove(3);        
-
-        print("isLegalMove: " + str(result))
-        self.assertFalse(result)
-
-    def test_isLegalMove_Player2_4(self):
-        sut = Game();
-
-        result = sut.isLegalMove(4);        
-
-        print("isLegalMove: " + str(result))
-        self.assertFalse(result)
-
-    def test_isLegalMove_Player2_5(self):
-        sut = Game();
-
-        result = sut.isLegalMove(5);        
-
-        print("isLegalMove: " + str(result))
-        self.assertFalse(result)
-
-    def test_isLegalMove_Player2_6(self):
-        sut = Game();
-
-        result = sut.isLegalMove(6);        
-
-        print("isLegalMove: " + str(result))
-        self.assertFalse(result)
-
-    def test_isLegalMove_Player2_7(self):
-        sut = Game();
-
-        result = sut.isLegalMove(67);        
-
-        print("isLegalMove: " + str(result))
-        self.assertTrue(result)
-
-    def test_isLegalMove_Player2_8(self):
-        sut = Game();
-
-        result = sut.isLegalMove(8);        
-
-        print("isLegalMove: " + str(result))
-        self.assertTrue(result)
-
-    def test_isLegalMove_Player2_8(self):
-        sut = Game();
-
-        result = sut.isLegalMove(9);        
-
-        print("isLegalMove: " + str(result))
-        self.assertTrue(result)
-
-    def test_isLegalMove_Player2_10(self):
-        sut = Game();
-
-        result = sut.isLegalMove(10);        
-
-        print("isLegalMove: " + str(result))
-        self.assertTrue(result)
-
-    def test_isLegalMove_Player2_11(self):
-        sut = Game();
-
-        result = sut.isLegalMove(11);        
-
-        print("isLegalMove: " + str(result))
-        self.assertTrue(result)
-
-    def test_isLegalMove_Player2_12(self):
-        sut = Game();
-
-        result = sut.isLegalMove(12);        
-
-        print("isLegalMove: " + str(result))
-        self.assertTrue(result)
-
-    def test_isLegalMove_Player2_13(self):
-        sut = Game();
-
-        result = sut.isLegalMove(13);        
-
-        print("isLegalMove: " + str(result))
-        self.assertFalse(result)
-
-    def test_isLegalMove_Player2_14(self):
-        sut = Game();
-
-        result = sut.isLegalMove(14);        
-
-        print("isLegalMove: " + str(result))
-        self.assertFalse(result)
+view = GameView();
