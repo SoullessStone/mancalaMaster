dad4d6b3db0140eb9cab71a93b11b5c0578ea3b3
SoullessStone <micheldc@windowslive.com>
2018-04-11 19:58:20 +0200

    baum aufbauen

--- treemodel.py
+++ treemodel.py
@@ -1,34 +1,37 @@
 class Tree:
-    __divStatus = None;
+    __value = None;
     __gameStatus = None;
     __subTrees = [];
 
-    def __init__(self, div, game):
-        self.__divStatus = div;
-        self.__gameStatus = 0;
-
-    def addSubTree(self, node):
-        self.__subTrees.append(node);
-
-    def calculateTree(self):
-        possibleMoves = self.__gameStatus.getPossibleMoves();
+    def calculateTree(self, game, depth, maxDepth, parentNode):
+        self.__gameStatus = game;
+        if depth == maxdepth:
+            self.calculateLeaveValue();
+            return self.__value;
+        
+        possibleMoves = game.getPossibleMoves();
+        childValues = [];
         for move in possibleMoves:
-            if depth <= maxdepth:
-                # set new Depth
-                newDepth = deepcopy(depth) + 1;
-                # deepCopy of moveListe for new Liste
-                newmoveListe = deepcopy(moveListe);
-                # deepCopy game for Movement
-                newGame = deepcopy(game);
-                # do Movement
-                newGame.doMove(move);
-                # write move to Liste
-                newmoveListe.append(move);
-                # checkIfBetterMovement
-                div = newGame.gameModel.getFieldValue(newGame.gameModel.PLAYER2_BASE) - newGame.gameModel.getFieldValue(newGame.gameModel.PLAYER1_BASE)
-                newDiv = (div+divBefore)/2;
-                # Make Tree Object
-                node = Tree(newDiv,newGame);
-                currentNode.addSubTree(node);
-                # do next Step
-                self.doAlphaBeta(newGame, newDepth,maxdepth, newmoveListe, newDiv, node);
+            # set new Depth
+            newDepth = deepcopy(depth) + 1;
+            # deepCopy game for Movement
+            newGame = deepcopy(game);
+            # do Movement
+            newGame.doMove(move);
+            # Make Tree Object
+            node = Tree();
+            parentNode.addSubTree(node);
+            childValue = self.calculateTree(newGame, newDepth, maxDepth, node);
+            childValues.append(childValue);
+        if self.__gameStatus.isMaxTurn():
+            self.__value = max(childValues);
+            return max(childValues);
+        else:
+            self.__value = min(childValues);
+            return min(childValues);
+
+    def calculateLeaveValue(self):
+        # diff function whatever
+
+    def addSubTree(self):
+        # todo
