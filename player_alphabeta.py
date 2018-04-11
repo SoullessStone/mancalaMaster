ac5fae010d67ce1afa0dc717b5ca9bc766649e59
Yannick94 <yst1994@gmx.ch>
2018-04-11 19:08:45 +0200

    Alpha Beta First Runnable Version

--- player_alphabeta.py
+++ player_alphabeta.py
@@ -5,34 +5,32 @@ class AlphaBetaPlayer:
 
     __bestMoveListe = [];
 
-    __bestDiv = 0;
+    __bestDiv = -99;
 
     def setGame(self, game):
         self.currentGame = game;
 
     def doMove(self):
         if self.currentGame.isMinTurn():
-            self.doAlphaBeta(self.currentGame, 0, 2, []);
+            self.doAlphaBeta(self.currentGame, 0, 6, [], self.currentGame.gameModel.getFieldValue(self.currentGame.gameModel.PLAYER2_BASE) - self.currentGame.gameModel.getFieldValue(self.currentGame.gameModel.PLAYER1_BASE));
+        self.currentGame.doMove(self.__bestMoveListe[0]);
+        self.resetAlphaBeta();
 
 
 
-
-    def checkIfBetterMoveMax(self, moveListe, div):
+    def checkIfBetterMove(self, moveListe, div):
         if(self.__bestDiv < div):
             self.__bestDiv = div;
             self.__bestMoveListe = moveListe;
             print("Div is " + str(div) + " on Move " + str(moveListe));
 
-    def checkIfBetterMoveMin(self, moveListe, div):
-        if(self.__bestDiv>div):
-            self.__bestDiv = div;
-            self.__bestMoveListe = moveListe;
-            print("Div is " + str(div) + " on Move " + str(moveListe));
-
+    def resetAlphaBeta(self):
+        self.__bestMoveListe = [];
+        self.__bestDiv = -99;
 
 
     #momentan nur immer am schauen bester Max Move (logikfehler von mir) muss ich noch anpassen
-    def doAlphaBeta(self, game, depth, maxdepth, moveListe):
+    def doAlphaBeta(self, game, depth, maxdepth, moveListe, divBefore):
         possibleMoves = game.getPossibleMoves();
         for move in possibleMoves:
             if depth <= maxdepth:
@@ -48,10 +46,8 @@ class AlphaBetaPlayer:
                 newmoveListe.append(move);
                 # checkIfBetterMovement
                 div = newGame.gameModel.getFieldValue(newGame.gameModel.PLAYER2_BASE) - newGame.gameModel.getFieldValue(newGame.gameModel.PLAYER1_BASE)
-                if newGame.isMinTurn():
-                    self.checkIfBetterMoveMin(newmoveListe, div);
-                else:
-                    self.checkIfBetterMoveMax(newmoveListe, div);
-                self.doAlphaBeta(newGame, newDepth,maxdepth, newmoveListe);
+                newDiv = (div+divBefore)/2;
+                self.checkIfBetterMove(newmoveListe, newDiv);
+                self.doAlphaBeta(newGame, newDepth,maxdepth, newmoveListe, newDiv);
                 
         
