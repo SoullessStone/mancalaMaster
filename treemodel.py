d4504404f60a651236b61077222a9ce68cf35b7a
Yannick94 <yst1994@gmx.ch>
2018-04-11 19:38:23 +0200

    Tree Model enhancment

--- treemodel.py
+++ treemodel.py
@@ -9,3 +9,26 @@ class Tree:
 
     def addSubTree(self, node):
         self.__subTrees.append(node);
+
+    def calculateTree(self):
+        possibleMoves = self.__gameStatus.getPossibleMoves();
+        for move in possibleMoves:
+            if depth <= maxdepth:
+                # set new Depth
+                newDepth = deepcopy(depth) + 1;
+                # deepCopy of moveListe for new Liste
+                newmoveListe = deepcopy(moveListe);
+                # deepCopy game for Movement
+                newGame = deepcopy(game);
+                # do Movement
+                newGame.doMove(move);
+                # write move to Liste
+                newmoveListe.append(move);
+                # checkIfBetterMovement
+                div = newGame.gameModel.getFieldValue(newGame.gameModel.PLAYER2_BASE) - newGame.gameModel.getFieldValue(newGame.gameModel.PLAYER1_BASE)
+                newDiv = (div+divBefore)/2;
+                # Make Tree Object
+                node = Tree(newDiv,newGame);
+                currentNode.addSubTree(node);
+                # do next Step
+                self.doAlphaBeta(newGame, newDepth,maxdepth, newmoveListe, newDiv, node);
